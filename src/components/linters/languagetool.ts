e870a93075fb02ac3f2dbb8f83603d5cf3aa103b
Pierre Marchand <pierre.jimthefrench@gmail.com>
2019-10-14 18:30:19 +0100

    code action added for LanguageTool

--- src/components/linters/languagetool.ts
+++ src/components/linters/languagetool.ts
@@ -7,76 +7,12 @@ interface ILanguageToolJSON {
     readonly shortMessage: string
     readonly offset: number
     readonly length: number
-    readonly replacement: {value : string}
-    readonly context: {text: string, offset: number, length: number}
-    readonly sentence: string
-    readonly rule: {id: string, subId: string, description: string, urls: {value: string}, issueType:string, category:{id:string,name:string}}
+    readonly replacements: {value : string}[]
+    // readonly context: {text: string, offset: number, length: number}
+    // readonly sentence: string
+    // readonly rule: {id: string, subId: string, description: string, urls: {value: string}, issueType:string, category:{id:string,name:string}}
 }
 
-// const ISSUE_MAP: {
-//     conditions: { Check?: RegExp; Description?: RegExp; Message?: RegExp }
-//     implications: (issue: ILanguageToolJSON) => { tags?: vscode.DiagnosticTag[]; replacement?: string }
-// }[] = [
-//     {
-//         conditions: {
-//             Message: /^(?:Consider using|Prefer|Use) '(.+)' (?:instead|over)(?: of)? '(.+)'$/i
-//             // catches: TheEconomist.(Terms,Punctuation),
-//             // PlainLanguage.(ComplexWords,Contractions,Wordiness,Words)
-//             // proselint.(AnimalLabels,Diacritical,GenderBias,GroupTerms,Nonwords)
-//             // 18F.(Abbreviations,Brands,Contractions,Terms)
-//         },
-//         implications: issue => {
-//             const match = issue.Message.match(/(?:Consider using|Prefer|Use) '(.+)' (?:instead|over)(?: of)? '(.+)'$/i)
-//             if (match) {
-//                 let replacement = match[1]
-//                 // make capitalisation match
-//                 if (match[2][0].toUpperCase() === match[2][0]) {
-//                     replacement = replacement[0].toUpperCase() + replacement.slice(1)
-//                 }
-//                 return { replacement, tags: [vscode.DiagnosticTag.Deprecated] }
-//             } else {
-//                 return {}
-//             }
-//         }
-//     },
-//     {
-//         conditions: {
-//             Check: /^LanguageTool\.Editorializing|write-good\.(?:So|Illusions)|TheEconomist\.UnnecessaryWords|proselint\.But/
-//         },
-//         implications: _issue => ({ tags: [vscode.DiagnosticTag.Deprecated], replacement: '' })
-//     },
-//     {
-//         conditions: {
-//             Message: /^Avoid using '(.+)'$/i
-//             // catches most of Joblint
-//         },
-//         implications: _issue => ({ tags: [vscode.DiagnosticTag.Unnecessary] })
-//     },
-//     {
-//         conditions: {
-//             Check: /^write-good\.Weasel/
-//         },
-//         implications: _issue => ({ tags: [vscode.DiagnosticTag.Unnecessary] })
-//     }
-// ]
-
-// function findIssueImplications(issue: ILanguageToolJSON) {
-//     for (let i = 0; i < ISSUE_MAP.length; i++) {
-//         const item = ISSUE_MAP[i]
-//         let meetsConditions = true
-//         for (const condition in item.conditions) {
-//             // @ts-ignore tslint is stupid, item.conditions[condition] is fine
-//             if (!item.conditions[condition].test(issue[condition])) {
-//                 meetsConditions = false
-//                 break
-//             }
-//         }
-//         if (meetsConditions) {
-//             return item.implications(issue)
-//         }
-//     }
-//     return undefined
-// }
 
 export const LanguageTool: IDiagnosticSource = {
     command: (fileName: string) => ['languagetool', '--json',fileName],
@@ -104,8 +40,7 @@ export const LanguageTool: IDiagnosticSource = {
             // accordingly
             const range = new vscode.Range(document.positionAt(issue.offset), document.positionAt(issue.offset+issue.length))
             const message = issue.message
-                // ? `${issue.message} (${issue.Check}, see ${issue.Link})`
-                // : `${issue.message} (${issue.Check})`
+
             let diagnostic = new vscode.Diagnostic(
                 range,
                 message,
@@ -117,7 +52,19 @@ export const LanguageTool: IDiagnosticSource = {
             )
             diagnostic.source = 'LanguageTool'
             // diagnostic.code = issue.Check
-            // diagnostic = this.applyIssueImplications(document, diagnostic, issue)
+
+            if (Object.keys(issue.replacements).length !== 0 ) {
+                for (const replacement of issue.replacements){
+                    const codeAction = new vscode.CodeAction(`Replace with '${replacement.value}'`,vscode.CodeActionKind.QuickFix)
+                    codeAction.command = {
+                        title: 'Replace value',
+                        command: 'latex-utilities.code-action',
+                        arguments: [document, diagnostic.range, diagnostic.source, replacement.value]
+                    }
+                    this.actions.set(diagnostic.range, codeAction)
+                }
+            }
+            
             diagnostics.push(diagnostic)
         }
 
@@ -127,30 +74,4 @@ export const LanguageTool: IDiagnosticSource = {
 
         this.diagnostics.set(document.uri, diagnostics)
     },
-    // applyIssueImplications: function(
-    //     document: vscode.TextDocument,
-    //     diagnostic: vscode.Diagnostic,
-    //     issue: ILanguageToolJSON
-    // ): vscode.Diagnostic {
-    //     const implications = findIssueImplications(issue)
-    //     if (implications !== undefined) {
-    //         if (implications.tags !== undefined) {
-    //             diagnostic.tags = implications.tags
-    //         }
-    //         if (implications.replacement !== undefined) {
-    //             const codeAction = new vscode.CodeAction(
-    //                 implications.replacement === '' ? 'Remove' : `Replace with '${implications.replacement}'`,
-    //                 vscode.CodeActionKind.QuickFix
-    //             )
-    //             codeAction.command = {
-    //                 title: 'Replace value',
-    //                 command: 'latex-utilities.code-action',
-    //                 arguments: [document, diagnostic.range, diagnostic.source, implications.replacement]
-    //             }
-    //             codeAction.isPreferred = true
-    //             this.actions.set(diagnostic.range, codeAction)
-    //         }
-    //     }
-    //     return diagnostic
-    // }
 }
