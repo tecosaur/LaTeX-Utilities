df7f91c55e9188d81759404dfaf6dd3a24371396
tecosaur <20903656+tecosaur@users.noreply.github.com>
2019-12-01 21:11:05 +0800

    Add getClosingBracket util function

--- src/utils.ts
+++ src/utils.ts
@@ -26,3 +26,46 @@ export function checkCommandExists(command: string) {
         }
     }
 }
+
+/**
+ * Find the position of the closing bracket { ... }
+ * @param s a string
+ * @param startIndex the position of the opening bracket
+ */
+export function getClosingBracket(s: string, startIndex: number = 0): number {
+    let nested = 0
+    let nestedSquare = 0
+    let i = startIndex
+    for (i; i < s.length; i++) {
+        switch (s[i]) {
+            case '{':
+                nested++
+                break
+            case '}':
+                nested--
+                break
+            case '\\':
+                // skip an escaped character
+                i++
+                break
+            default:
+        }
+        switch (s[i]) {
+            case '[':
+                nestedSquare++
+                break
+            case ']':
+                nestedSquare--
+                break
+            case '\\':
+                // skip an escaped character
+                i++
+                break
+            default:
+        }
+        if (nested === 0 && nestedSquare === 0) {
+            break
+        }
+    }
+    return i
+}
